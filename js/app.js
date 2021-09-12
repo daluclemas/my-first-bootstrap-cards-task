e4ed585506e3216df41f6b68b88cb2ab49bc4eb0
daluclemas <chidaluanukam@gmail.com>
2021-09-12 15:01:12 -0700

    pushing my first bootstrap project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelectorAll('.btn');
+
+// console.log(btn);
+
+const makeArr = Array.from(btn);
+
+// console.log(makeArr)
+
+makeArr.forEach(item=>{
+
+    item.addEventListener('click', (e)=>{
+        // change the font color
+
+        e.target.classList.toggle('active');
+        
+        // remove the white background and add the white border
+
+        e.target.parentElement.classList.toggle('active');
+
+    });
+
+});
